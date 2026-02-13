a502838b9a8fa8ed8a43ca05f9e075d4a7b78e95
Sharon Moegi <sharamoegi@gmail.com>
2026-02-13 15:24:56 +0300

    Add ASL symbol for 'My name is'

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function Translate() {
         return; 
     }
 
+    //ASL Alphabet
     const aslAlphabet = {
          a: "images/asl/a.png", b: "images/asl/b.png", c: "images/asl/c.png", d: "images/asl/d.png", 
          e: "images/asl/e.png", f: "images/asl/f.png", g: "images/asl/g.png", h: "images/asl/h.png", 
@@ -14,5 +15,20 @@ function Translate() {
          m: "images/asl/m.png", n: "images/asl/n.png", o: "images/asl/o.png", p: "images/asl/p.png", 
          q: "images/asl/q.png", r: "images/asl/r.png", s: "images/asl/s.png", t: "images/asl/t.png", 
          u: "images/asl/u.png", v: "images/asl/v.png", w: "images/asl/w.png", x: "images/asl/x.png", 
-         y: "images/asl/y.png", z: "images/asl/z.png" };
+         y: "images/asl/y.png", z: "images/asl/z.png",
+        
+        };
+
+    let myname = "<h3>INTRO</h3>";
+    myname += `
+    <div>
+    <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmVvdHpkNGNleGllMnhzYW44NnpnY3lpMmkwZXkxOXZ2b3QweWFpNCZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/StKsUzn4txjJlX2Zil/giphy.webp"
+    alt="My name is" style="width:120px; height:auto";
+    <p><strong>My name is</strong></p>
+
+    </div>
+    `;
+    
+
+    
 }
