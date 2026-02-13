d06217e55055a35a1d2b16b58628f5dfb7b50d38
Sharon Moegi <sharamoegi@gmail.com>
2026-02-13 15:00:01 +0300

    Add DOM elements

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function Translate() {
+     const nameInput = document.getElementById("name").value.trim(); 
+     const outputSection = document.getElementById("cards"); 
+    //  Data Validation
+     if (!nameInput) { 
+        outputSection.innerHTML = "<p>Please enter a name.</p>"; 
+        return; 
+    }
+}
